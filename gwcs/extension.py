04c1753fc8ceaff376eaeed4be33a3537a536ecb
Michael Droettboom <mdboom@gmail.com>
2015-09-17 16:49:13 -0400

    Fix extension

--- gwcs/extension.py
+++ gwcs/extension.py
@@ -1,21 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import, division, unicode_literals, print_function
 
-from pyasdf.extension import AsdfExtension
+from pyasdf.extension import BuiltinExtension
 from pyasdf import util, resolver
 from .tags import LabelMapperType, RegionsSelectorType
 
 
-class GWCSExtension(AsdfExtension):
+class GWCSExtension(BuiltinExtension):
     @property
     def types(self):
         return [LabelMapperType, RegionsSelectorType]
-
-    @property
-    def tag_mapping(self):
-        return [('tag:stsci.edu:asdf/',
-                 'http://stsci.edu/schemas/asdf{tag_suffix}')]
-
-    @property
-    def url_mapping(self):
-        return resolver.DEFAULT_URL_MAPPING
