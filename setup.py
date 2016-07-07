407c7c0b4d3b2cbb6c5fa3532e5ec9795532755a
Pey Lian Lim <lim@stsci.edu>
2016-07-07 14:21:07 -0400

    Updated submodule. Fixed configparser Python 3 compatibility.

--- setup.py
+++ setup.py
@@ -22,7 +22,11 @@ from astropy_helpers.version_helpers import generate_version_py
 
 # Get some values from the setup.cfg
 from distutils import config
-conf = config.ConfigParser()
+try:
+    from ConfigParser import ConfigParser
+except ImportError:
+    from configparser import ConfigParser
+conf = ConfigParser()
 conf.read(['setup.cfg'])
 metadata = dict(conf.items('metadata'))
 
