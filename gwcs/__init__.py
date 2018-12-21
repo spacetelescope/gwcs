dfc7ef938f7f394bf52062496aab747f4daa7606
Nadia Dencheva <nadia.dencheva@gmail.com>
2018-12-21 10:38:27 -0500

    update package docs

--- gwcs/__init__.py
+++ gwcs/__init__.py
@@ -4,21 +4,33 @@
    GWCS - Generalized World Coordinate System
    ==========================================
 
-Generalized World Coordinate System (GWCS) is an `Astropy`_ affiliated package providing tools for managing the World Coordinate System of astronomical data.
 
-GWCS takes a general approach to the problem of expressing transformations between pixel and world coordinates. It supports a data model which includes the entire transformation pipeline from input coordinates (detector by default) to world coordinates. It is tightly integrated with `Astropy`_.
+Generalized World Coordinate System (GWCS) is an Astropy affiliated package
+providing tools for managing the World Coordinate System of astronomical data.
 
-- Transforms are instances of ``astropy.Model``. They can be chained, joined or combined with arithmetic operators using the flexible framework of compound models in `astropy.modeling`_.
-- Celestial coordinates are instances of ``astropy.SkyCoord`` and are transformed to other standard celestial frames using `astropy.coordinates`_.
-- Time coordinates are represented by ``astropy.Time`` and can be further manipulated using the tools in `astropy.time`_
-- Spectral coordinates are ``astropy.Quantity`` objects and can be converted to other units using the tools in `astropy.units`_.
+GWCS takes a general approach to the problem of expressing transformations between
+pixel and world coordinates. It supports a data model which includes the entire
+transformation pipeline from input coordinates (detector by default) to world
+coordinates. It is tightly integrated with Astropy.
 
-For complete features and usage examples see the `documentation`_ site.
+- Transforms are instances of ``astropy.Model``. They can be chained, joined or
+  combined with arithmetic operators using the flexible framework of
+  compound models in ``astropy.modeling``.
+- Celestial coordinates are instances of ``astropy.SkyCoord`` and are transformed
+  to other standard celestial frames using ``astropy.coordinates``.
+- Time coordinates are represented by ``astropy.Time`` and can be further manipulated
+  using the tools in ``astropy.time``
+- Spectral coordinates are ``astropy.Quantity`` objects and can be converted to other
+  units using the tools in ``astropy.units``.
+
+For complete features and usage examples see the documentation site:
+
+http://gwcs.readthedocs.org
 
 Note
 ----
 
-GWCS support only Python 3.
+GWCS supports only Python 3.
 
 
 Installation
@@ -38,22 +50,10 @@ To clone from github and install the master branch::
 Contributing Code, Documentation, or Feedback
 ---------------------------------------------
 
-We welcome feedback and contributions to the project. Contributions of
-code, documentation, or general feedback are all appreciated. Please
-follow the `contributing guidelines <CONTRIBUTING.md>`__ to submit an
-issue or a pull request.
+GWCS is developed on github. We welcome feedback and contributions to the project.
+Contributions of code, documentation, or general feedback are all appreciated. More
+information about contributing is in the github repository.
 
-We strive to provide a welcoming community to all of our users by
-abiding to the `Code of Conduct <CODE_OF_CONDUCT.md>`__.
-
-
-.. _Astropy: http://www.astropy.org/
-
-.. _astropy.time: http://docs.astropy.org/en/stable/time/
-.. _astropy.modeling: http://docs.astropy.org/en/stable/modeling/
-.. _astropy.units: http://docs.astropy.org/en/stable/units/
-.. _astropy.coordinates: http://docs.astropy.org/en/stable/coordinates/
-.. _documentation: http://gwcs.readthedocs.org/en/latest/
 
 """
 
