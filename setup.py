8318195d08bb8074d4b529c99425ced6c7d86538
Zach Burnett <zburnett@stsci.edu>
2022-12-12 11:44:29 -0500

    move configuration into `pyproject.toml`

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-from setuptools import setup, find_packages
-
-setup(use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      packages=find_packages(),
-)
