Fix units in WCS API world_axis_object_components (#512)
Co-authored-by: Nadia Dencheva <nadia.dencheva@gmail.com>

gwcs/tests/test_api.py
@@ -165,8 +165,10 @@ def test_array_index_to_world_values(gwcs_2d_spatial_shift, x, y):
 
 def test_world_axis_object_components_2d(gwcs_2d_spatial_shift):
     waoc = gwcs_2d_spatial_shift.world_axis_object_components
-    assert waoc == [('celestial', 0, 'spherical.lon'),
-                    ('celestial', 1, 'spherical.lat')]
+    assert waoc[0][:2] == ('celestial', 0)
+    assert callable(waoc[0][2])
+    assert waoc[1][:2] == ('celestial', 1)
+    assert callable(waoc[1][2])
 
 
 def test_world_axis_object_components_2d_generic(gwcs_2d_quantity_shift):
@@ -177,15 +179,19 @@ def test_world_axis_object_components_2d_generic(gwcs_2d_quantity_shift):
 
 def test_world_axis_object_components_1d(gwcs_1d_freq):
     waoc = gwcs_1d_freq.world_axis_object_components
-    assert waoc == [('spectral', 0, 'value')]
+    assert [c[:2] for c in waoc] == [('spectral', 0)]
+    assert callable(waoc[0][2])
 
 
 def test_world_axis_object_components_4d(gwcs_4d_identity_units):
     waoc = gwcs_4d_identity_units.world_axis_object_components
-    assert waoc[0:3] == [('celestial', 0, 'spherical.lon'),
-                         ('celestial', 1, 'spherical.lat'),
-                         ('spectral', 0, 'value')]
-    assert waoc[3][0:2] == ('temporal', 0)
+    first_two = [c[:2] for c in waoc]
+    last_one = [c[2] for c in waoc]
+    assert first_two == [('celestial', 0),
+                         ('celestial', 1),
+                         ('spectral', 0),
+                         ('temporal', 0)]
+    assert all([callable(l) for l in last_one])
 
 
 def test_world_axis_object_classes_2d(gwcs_2d_spatial_shift):
@@ -521,3 +527,19 @@ def test_coordinate_frame_api():
 
     pixel2 = wcs.invert(world)
     assert u.allclose(pixel2, 0*u.pix)
+
+
+def test_world_axis_object_components_units(gwcs_3d_identity_units):
+    from astropy.wcs.wcsapi.high_level_api import high_level_objects_to_values
+
+    wcs = gwcs_3d_identity_units
+    world = wcs.pixel_to_world(1, 1, 1)
+
+    values = high_level_objects_to_values(*world, low_level_wcs=wcs)
+
+    expected_values = [world[0].spherical.lon.to_value(wcs.output_frame.unit[0]),
+                       world[0].spherical.lon.to_value(wcs.output_frame.unit[1]),
+                       world[1].to_value(wcs.output_frame.unit[2])]
+
+    assert not any([isinstance(o, u.Quantity) for o in values])
+    np.testing.assert_allclose(values, expected_values)