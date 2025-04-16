9c7944b7cfe56efc4f36c089b7106d57820d8965
William Jamieson <william@jamieson.phd>
2025-04-16 16:25:47 -0400

    Switch to using furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -44,13 +44,6 @@ except ImportError:
 
 # -- General configuration ----------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-# needs_sphinx = '1.2'
-
-# To perform a Sphinx version check that needs to be more specific than
-# major.minor, call `check_sphinx_version("x.y.z")` here.
-# check_sphinx_version("1.2.1")
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns.append("_templates")  # noqa: F405
@@ -77,6 +70,7 @@ asdf_schema_reference_mappings = [
 with (Path(__file__).parent.parent / "pyproject.toml").open("rb") as metadata_file:
     configuration = tomllib.load(metadata_file)
     metadata = configuration["project"]
+
 project = metadata["name"]
 author = metadata["authors"][0]["name"]
 copyright = f"{datetime.now().year}, {author}"  # noqa: A001
@@ -90,58 +84,21 @@ release = distribution(project).version
 version = ".".join(release.split(".")[:2])
 
 # -- Options for HTML output ---------------------------------------------------
-
-# A NOTE ON HTML THEMES
-# The global astropy configuration uses a custom theme, 'bootstrap-astropy',
-# which is installed along with astropy. A different theme can be used or
-# the options for this theme can be modified by overriding some of the
-# variables set in the global configuration. The variables set in the
-# global configuration are listed below, commented out.
-
-# Add any paths that contain custom themes here, relative to this directory.
-# To use a different custom theme, add the directory containing the theme.
-# html_theme_path = []
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes. To override the custom theme, set this to the
-# name of a builtin theme or the name of a custom theme in html_theme_path.
-# html_theme = None
-
-
-# Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = ''
-
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = ''
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
 html_title = f"{project} v{release}"
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = project + "doc"
+htmlhelp_basename = f"{project}doc"
 
 # -- Options for LaTeX output --------------------------------------------------
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
-# latex_documents = [('index', project + '.tex', project + u' Documentation',
-#                    author, 'manual')]
-
+latex_documents = [
+    ("index", project + ".tex", project + " Documentation", author, "manual")
+]
+latex_logo = "_static/images/logo-light-mode.png"
 
 # -- Options for manual page output --------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
 man_pages = [("index", project.lower(), project + " Documentation", [author], 1)]
 
-sys.path.insert(0, str(Path(__file__).parent / "sphinxext"))
+# -- Add in additional sphinx extensions ----------------------------------------
 extensions += ["sphinx_asdf"]  # noqa: F405
 
 # Enable nitpicky mode - which ensures that all references in the docs resolve.
@@ -152,4 +109,40 @@ nitpick_ignore = [
     ("py:obj", "astropy.modeling.projections.projcodes"),
     ("py:attr", "gwcs.WCS.bounding_box"),
     ("py:meth", "gwcs.WCS.footprint"),
+]
+
+# -- Options for html theme -------------------------------------------------
+html_theme = "furo"
+html_static_path = ["_static"]
+# Override default settings from sphinx_asdf / sphinx_astropy (incompatible with furo)
+html_sidebars = {}
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+# html_favicon = "_static/images/favicon.ico"
+html_logo = ""
+
+# html_theme_options = {
+#     "light_logo": "images/logo-light-mode.png",
+#     "dark_logo": "images/logo-dark-mode.png",
+# }
+html_theme_options = {
+    "light_logo": "images/stsci_logo.png",
+    "dark_logo": "images/stsci_logo.png",
+}
+
+pygments_style = "monokai"
+# NB Dark style pygments is furo-specific at this time
+pygments_dark_style = "monokai"
+# Render inheritance diagrams in SVG
+graphviz_output_format = "svg"
+
+graphviz_dot_args = [
+    "-Nfontsize=10",
+    "-Nfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Efontsize=10",
+    "-Efontname=Helvetica Neue, Helvetica, Arial, sans-serif",
+    "-Gbgcolor=white",
+    "-Gfontsize=10",
+    "-Gfontname=Helvetica Neue, Helvetica, Arial, sans-serif",
 ]
